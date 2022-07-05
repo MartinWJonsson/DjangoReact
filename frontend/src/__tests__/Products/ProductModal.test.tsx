bddb18d5873599450c49113298c58a4f9619d223
MartinWJonsson <martin.jonsson@octanelending.com>
2022-07-05 10:09:50 +0200

    changed the gitignore

--- frontend/src/__tests__/Products/ProductModal.test.tsx
+++ frontend/src/__tests__/Products/ProductModal.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor, act } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { resolve } from 'path';
 import { Provider } from 'react-redux';
 import { store } from '../../app/store';
